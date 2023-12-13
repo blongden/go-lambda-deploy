98e6dac0d3479b5edb19cde8dc2d8a387adf4f40
Ben Longden <ben.longden@infinityworks.com>
2023-12-13 13:16:58 +0000

    Fix expected test output

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ func TestHandler(t *testing.T) {
 
 	message, _ := HandleRequest(ctx, &event)
 
-	if *message != "Goodbye, world!" {
+	if *message != "Hello, world!" {
 		t.Errorf("Unexpected output: %q", *message)
 	}
 }
